10d160b97c0a054c530dd2bcb5099ba79d5b0c81
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-03-19 15:18:33 +0900

    init setting

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- <?php 
+
+echo "php 공부합니당🤗❤️"; 
+
+?>  -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<h1>뭐양</h1>
+	<?php
+	// 변수
+	$a = 1;
+	 echo "이렇게 쓸수 있다고? "; 
+	?>
+	<script>
+		// 변수
+		let a = 1;
+		document.write("오늘은 피티가는 날!")
+	</script>
+</body>
+</html>
