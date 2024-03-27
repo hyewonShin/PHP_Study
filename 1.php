d1bd7da665578c03b9ba1cb385832fc85c8194f7
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-03-27 17:24:15 +0900

    PHP 변수의 범위, 지역변수, 글로벌 변수 등

--- 1.php
+++ 1.php
@@ -12,14 +12,34 @@
     ?>
   </body>
 </html>
+  $txt = "안녕하세요"; // 문자열
+  $x = 5; //정수
+  $y = 10.5; //실수
 
-<?php 
-    // xampp
-    // 웹서버 , DB서버 , PHP
-    // Appache, Mariadb, PHP 컴파일러 
+  // 변수는 대소문자를 구분한다.
+  echo "x = $x, X= $X <br>";
+  ?>
 
-    $txt = "PHP";
-    echo "I love $txt !"; 
+  <?php
+  // 전역변수
+  // 로컬변수(지역변수)
+  $x = 5;
+  
+  function myTest() {
+    // 함수안에서 사용하려면 해당 변수를 앞에 global을 붙여준다.
+    global $x;
+    $y = 111;
+    $x++;
+    echo "함수 안 변수 x의 출력값 : $x <br>"; 
+    echo "함수 안 변수 y의 출력값 : $y <br>"; 
+    echo "<br>";
+  }
+
+  echo "함수 밖 변수 x의 출력값 : $x <br>";
+  
+  myTest();
+
 ?>
 
-
+</body>
+</html>
