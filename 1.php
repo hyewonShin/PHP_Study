660628c8d4f25fd8c8f38d7fffc419810e85d586
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-03-27 16:24:59 +0900

    PHP 생기초문법, 변수표기법, 주석처리, 자바스크립트와 차이

---
+++ 1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>My first PHP page</h1>
+    <?php
+      echo "hello World!";
+    ?>
+  </body>
+</html>
+
+<?php 
+    // xampp
+    // 웹서버 , DB서버 , PHP
+    // Appache, Mariadb, PHP 컴파일러 
+
+    $txt = "PHP";
+    echo "I love $txt !"; 
+?>
+
+
